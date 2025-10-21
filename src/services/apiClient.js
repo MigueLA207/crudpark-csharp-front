22fe3cdf1747d4a99e0de1875f38ffbbf04acc55
marianarestrepo26 <marianartpo08@gmail.com>
2025-10-21 15:52:03 -0500

    FEAT: connect back and front

--- src/services/apiClient.js
+++ src/services/apiClient.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-    baseURL: 'https://crudpark-csharp-back-1hes.onrender.com/', // <-- ¡URL de tu back-end!
+    baseURL: 'https://crudpark-csharp-back-1hes.onrender.com/',
     headers: {
         'Content-Type': 'application/json'
     }
